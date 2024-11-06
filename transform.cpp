e23d8bbdb3091e2bfad816e350a393becfd72ccf
cuppajoeman <ccn@cuppajoeman.com>
2024-11-06 15:51:16 -0500

    feat!: update transform api for easier usage

--- transform.cpp
+++ transform.cpp
@@ -2,17 +2,17 @@
 
 Transform::Transform() : position(0.0f, 0.0f, 0.0f), rotation(0.0f, 0.0f, 0.0f), scale(1.0f, 1.0f, 1.0f) {}
 
-const glm::vec3 &Transform::get_position() const { return position; }
-
-const glm::vec3 &Transform::get_rotation() const { return rotation; }
-
-const glm::vec3 &Transform::get_scale() const { return scale; }
-
-void Transform::set_position(const glm::vec3 &pos) { position = pos; }
-
-void Transform::set_rotation(const glm::vec3 &rot) { rotation = rot; }
-
-void Transform::set_scale(const glm::vec3 &scl) { scale = scl; }
+/*const glm::vec3 &Transform::get_position() const { return position; }*/
+/**/
+/*const glm::vec3 &Transform::get_rotation() const { return rotation; }*/
+/**/
+/*const glm::vec3 &Transform::get_scale() const { return scale; }*/
+/**/
+/*void Transform::set_position(const glm::vec3 &pos) { position = pos; }*/
+/**/
+/*void Transform::set_rotation(const glm::vec3 &rot) { rotation = rot; }*/
+/**/
+/*void Transform::set_scale(const glm::vec3 &scl) { scale = scl; }*/
 
 glm::mat4 Transform::get_transform_matrix() const {
     glm::mat4 translate = glm::translate(glm::mat4(1.0f), position);
@@ -28,4 +28,12 @@ void Transform::print() const {
     std::cout << "Position: (" << position.x << ", " << position.y << ", " << position.z << ")\n"
               << "Rotation: (" << rotation.x << ", " << rotation.y << ", " << rotation.z << ")\n"
               << "Scale: (" << scale.x << ", " << scale.y << ", " << scale.z << ")\n";
+}
+
+glm::vec3 Transform::compute_forward_vector() const {
+    glm::vec3 forward;
+    forward.x = cos(glm::radians(rotation.x)) * cos(glm::radians(rotation.y));
+    forward.y = sin(glm::radians(rotation.x));
+    forward.z = cos(glm::radians(rotation.x)) * sin(glm::radians(rotation.y));
+    return glm::normalize(forward);
 }
--- transform.hpp
+++ transform.hpp
@@ -9,21 +9,9 @@
 class Transform {
   public:
     Transform();
-
-    // avoiding copying while making sure they canot be modified
-    const glm::vec3 &get_position() const;
-    const glm::vec3 &get_rotation() const;
-    const glm::vec3 &get_scale() const;
-
-    void set_position(const glm::vec3 &pos);
-    void set_rotation(const glm::vec3 &rot);
-    void set_scale(const glm::vec3 &scl);
-
     glm::mat4 get_transform_matrix() const;
-
+    glm::vec3 compute_forward_vector() const;
     void print() const;
-
-  private:
     glm::vec3 position; // Position in 3D space
     glm::vec3 rotation; // Euler angles in degrees (pitch, yaw, roll)
     glm::vec3 scale;    // Scale factors
